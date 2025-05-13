92693747c1d3dcf887700baf40c334a8e2f69b15
shel.v3 <shelestynvadyn123.na.v@gmail.com>
2025-05-13 15:03:12 +0300

    Removed old file. Added new files

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import NexcentLandingPage from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NexcentLandingPage />
+  </React.StrictMode>,
+);
